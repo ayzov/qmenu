3fd70916c90b53c2530e05b484db295b976e742b
ay.ru <157128936+ayzov@users.noreply.github.com>
2025-06-26 12:43:34 +0300

    Add files via upload

---
+++ html/js/rofls.js
@@ -0,0 +1,32 @@
+var gScope = null;
+var GamemodeDetails = {}
+var MapIndex = {}
+var AnnSpeed=300;
+var menuversion="1321";
+var bannerurl="https://gaerisson-softs.fr/logos/gmodmenu/banner.php?ver="+menuversion;
+
+setTimeout(function(){
+	console.log( "#########################################################" );
+	console.log( "" );
+	console.log( "         ~~                                                 " );
+	console.log( "    ~~#####~  -                                             " );
+	console.log( "  ##########~##*~~~                                         " );
+	console.log( "  ~##########~~ #######~~                                   " );
+	console.log( "   #####~~###   #~#######~                                  " );
+	console.log( "   _###~  ###~ -  -######    ######## ######## ##-    ~#~   " );
+	console.log( "    ####- ~   _#########~   ~#      * #-       ####  ####   " );
+	console.log( "     ~   ~~~  ~#########    ~#    ##~ #        #  #### ##   " );
+	console.log( "         ~~~   -~~######    ~#    -~# #        #~  ~~  ##   " );
+	console.log( "     -#~~~~~~~~####~ ~###   ~######## ######## #~      ##   " );
+	console.log( "     -~~~~~~~~~####~ ####     ~~~~~~   ~~~~~~~              " );
+	console.log( "     -~~~~~~#############                                   " );
+	console.log( "     -~~~~~~~~~##########                                   " );
+	console.log( "     -##~~~~~~~##########                                   " );
+	console.log( "      ___-_--- ~~~~~~~~~                                    " );
+	console.log( "" );
+	console.log( "#########################################################" );
+	console.log( "############## Gmod Custom Menu > Loaded ! ##############" );
+	console.log( "#################### Version > B"+menuversion+" ####################" );
+	console.log( "##### https://github.com/Gaerisson/Gmod-Custom-Menu #####" );
+	console.log( "###### Don't forget to check for update manually ! ######" );
+	console.log( "#########################################################" );
